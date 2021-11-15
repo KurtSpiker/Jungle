c1551faef4e5837d7e2e9013a6e686c6efe963a5
KurtSpiker <kurt.spiker@gmail.com>
2021-11-15 02:52:12 -0700

    Working login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-  resources :sessions, only: [:new, :create, :delete]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
