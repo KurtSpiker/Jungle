b873792c979bb7e9efc5a9052d49b49abe836256
KurtSpiker <kurt.spiker@gmail.com>
2021-11-15 02:15:52 -0700

    Sessions and user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  resources :sessions, only: [:new, :create, :delete]
+  resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
