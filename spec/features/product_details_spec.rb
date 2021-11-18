cc971b3cea71ed436619f5d23abe8b120eb2b878
KurtSpiker <kurt.spiker@gmail.com>
2021-11-17 17:44:24 -0700

    testing product page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Click on a product to see its details", type: :feature, js: true do
+
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+    scenario "Can click on one product and be redirected to product details page" do
+      #ACT
+      visit root_path
+
+      # page.find(".pull-right",  match: :first).click
+      page.all(".product .actions a")[0].click
+
+      #to wait to take screen shot
+      sleep 1
+
+      # commented out b/c it's for debugging only
+      save_screenshot
+
+      # VERIFY
+      expect(page).to have_css '.products-show'
+      expect(page).to have_css("img[src*='apparel1.jpg']")
+  end
 end
